18cdeeb0090c0425717f796746453468a13fc391
Om-Chavan501 <om.chavan501@gmail.com>
2024-09-28 15:36:31 +0530

    from community discussion

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Update package lists and install dependencies for Chrome
-apt-get update -y && apt-get install -y wget gnupg2
+STORAGE_DIR=/opt/render/project/.render
 
-# Add Google Chrome's GPG key and repository
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
 
-# Install Google Chrome
-apt-get update -y && apt-get install -y google-chrome-stable
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# add your own build commands...
